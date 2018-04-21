328bffe46f5727966a7633f29097242c1fd41cf3
lindhmik <32427584+lindhmik@users.noreply.github.com>
2018-04-21 17:30:33 +0300

    k

--- menu.js
+++ menu.js
@@ -13,3 +13,17 @@ function openRaportit(){
 	
 	document.location="raportit.php";
 }
+
+function modifyRow(id) {
+	var row = document.getElementById(id);
+	var personId= row.childNodes[1];
+	var date= row.childNodes[2];
+	var project= row.childNodes[3];
+	var hours= row.childNodes[4];
+	var overtime = row.childNodes[5];
+	var wkd= row.childNodes[6];
+	var km= row.childNodes[7];
+	var kmDes= row.childNodes[8];
+	
+	document.getElementById('pvm').value=date;
+}
