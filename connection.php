065426c51e29ea4986b2c1a9e50cc7750693a471
JonnaPartanen <33378336+JonnaPartanen@users.noreply.github.com>
2018-04-29 10:03:08 +0300

    update

--- connection.php
+++ connection.php
@@ -2,11 +2,16 @@
 
 function get_database(){
     
+    $dbhost= "127.0.0.1:49518";
+    $dbname= "localdb";
+    $dbusername= "azure";
+    $dbpassword= "6#vWHD_$";
     
-    $config = parse_ini_file('config.ini'); 
     
-    $mysqli = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
+    //$config = parse_ini_file('config.ini'); 
     
+   // $mysqli = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
+    $mysqli = new mysqli($dbhost, $dbname, $dbusername, $dbpassword);
     // Check connection
     if ($mysqli->connect_error) {
         return ("Connection failed: " . $myqli->connect_error);
